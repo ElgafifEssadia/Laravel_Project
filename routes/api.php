a15fcff20a95a7f324d4ffea58edda9bba52094a
MohamedLM <laamarti.lm.93@gmail.com>
2020-05-19 06:42:39 +0000

    fin project

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,15 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+
+Route::post('user/register', 'APIRegisterController@register');
+Route::post('user/login', 'APILoginController@login');
+
+// with token  Type Bearer Token
+Route::middleware('jwt.auth')->get('/users', function (Request $request) {
+    return auth()->user();
 });
+
+Route::middleware('jwt.auth')->group( function(){
+    Route::resource('products', 'API\ProductController') ;
+} );
